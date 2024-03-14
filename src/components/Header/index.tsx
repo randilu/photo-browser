a61bb82f56e3a0d028c27d772a3394b6ab83691c
randilu <randilu.soysa@gmail.com>
2024-03-14 11:57:32 +0200

    Add Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <div className="header-container">
+        <p className="header-label">Photo Browser 📷</p>
+    </div>
+  )
+}
