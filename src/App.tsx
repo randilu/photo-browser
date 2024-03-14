60f22560fa049df49b07125743ac59093e5fe8a9
randilu <randilu.soysa@gmail.com>
2024-03-14 12:37:32 +0200

    Create PhotoGrid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import PhotoGrid from "./components/PhotoGrid";
 
 function App() {
   return (
     <div>
       <Header />
+      <PhotoGrid />
     </div>
   );
 }
---
+++ src/components/PhotoGrid/index.tsx
@@ -0,0 +1,78 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import "./styles.css";
+
+const PhotoGrid = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [photos, setPhotos] = useState([]);
+
+  const pageSize = 20;
+
+  const observer = useRef<IntersectionObserver>();
+
+  const lastPhotoElmRef = useCallback(
+    (elm: HTMLDivElement) => {
+      if (isLoading) return;
+      if (observer.current) observer.current.disconnect();
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (hasNextPage && entries[0].isIntersecting) {
+          setCurrentPage((previousPage) => previousPage + 1);
+        }
+      });
+
+      if (elm) observer.current.observe(elm);
+    },
+    [isLoading, hasNextPage]
+  );
+
+  useEffect(() => {
+    async function fetchPhotos() {
+      setIsLoading(true);
+      let response;
+      try {
+        response = await fetch(
+          `https://jsonplaceholder.typicode.com/photos?_start=${currentPage}&_limit=${pageSize}`
+        );
+      } catch (error) {
+        console.error("Error fetching photos", error);
+        setIsLoading(false);
+        return;
+      }
+      const newPhotos = (await response.json()) as [];
+      setPhotos((photos) => [...photos, ...newPhotos]);
+      setHasNextPage(newPhotos.length > 0);
+      setIsLoading(false);
+    }
+
+    fetchPhotos();
+  }, [currentPage]);
+
+  const photoList = photos.map(
+    (photo: { id: string; thumbnailUrl: string }, index: number) => {
+      if (photos.length === index + 1) {
+        return (
+          <div key={photo.id} className="grid-item" ref={lastPhotoElmRef}>
+            <img src={photo.thumbnailUrl} alt="thumbnail" />
+          </div>
+        );
+      }
+
+      return (
+        <div key={photo.id} className="grid-item">
+          <img src={photo.thumbnailUrl} alt="thumbnail" />
+        </div>
+      );
+    }
+  );
+
+  return (
+    <div className="container">
+      <div className="grid-container">{photoList}</div>
+      {isLoading && <div className="loading">Loading...</div>}
+    </div>
+  );
+};
+
+export default PhotoGrid;
